a9519b6d4564a670837e97cd7dbf00b9b2082d12
kkrylov <kirill.krylov@geodis.com>
2022-11-05 21:14:52 +0100

    1

--- src/main/java/kk/springboothelloworldkk/RootController.java
+++ src/main/java/kk/springboothelloworldkk/RootController.java
@@ -1,11 +1,18 @@
 package kk.springboothelloworldkk;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
 @RequestMapping("/")
 @RestController
 public class RootController {
@@ -19,4 +26,24 @@ public class RootController {
     ResponseEntity getEnv(){
         return ResponseEntity.ok(environment);
     }
+
+    @Value("${toto}")
+    String toto;
+
+
+    @Autowired
+    public JavaMailSender javaMailSender;
+    @Scheduled(initialDelay = 1000, fixedDelay = 1000000000)
+    void test(){
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setTo(toto);
+        simpleMailMessage.setFrom(toto);
+        simpleMailMessage.setText(toto);
+        simpleMailMessage.setSubject(toto);
+        simpleMailMessage.setSentDate(new Date());
+
+        javaMailSender.send(simpleMailMessage);
+    }
+
+
 }
--- src/main/java/kk/springboothelloworldkk/SpringBootHelloworldKkApplication.java
+++ src/main/java/kk/springboothelloworldkk/SpringBootHelloworldKkApplication.java
@@ -1,8 +1,12 @@
 package kk.springboothelloworldkk;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class SpringBootHelloworldKkApplication {
 
